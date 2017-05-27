e3eb48c8da4481a3364620b39bbcae8d535d5c62
Martijn Terpstra <bigmartijn@gmail.com>
2017-05-27 21:47:11 +0200

    Added commentary

--- nubox-light-theme.el
+++ nubox-light-theme.el
@@ -1,9 +1,29 @@
 ;;; nubox-light-theme.el --- Nubox color theme (light version)
 
+;; Author: Martijn Terpstra <bigmartijn@gmail.com>
+;; Package-Version: 1.0.0
+;; Keywords: faces
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
 ;;; Commentary:
 ;;
 
 ;;; Code:
+
 
 (deftheme nubox-light "Personal theme (light version)")
 (let (
@@ -180,5 +200,7 @@
      `(show-paren-match ((t (:weight bold :foreground ,white :background ,black2))))
      `(show-paren-mismatch ((t (:weight bold :foreground ,black :background ,red)))))))
 (provide-theme 'nubox-light)
+
+(provide 'nubox-light-theme)
 
 ;;; nubox-light-theme.el ends here
